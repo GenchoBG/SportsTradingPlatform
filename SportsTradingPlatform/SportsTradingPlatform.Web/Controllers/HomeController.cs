e70bcfaf8a8352bda2e78bc1a2d110f16571c810
GenchoBG <genchobg11@gmail.com>
2019-06-15 19:48:08 +0300

    Add base project generated from Visual Studio

---
+++ SportsTradingPlatform/SportsTradingPlatform.Web/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsTradingPlatform.Web.Models;
+
+namespace SportsTradingPlatform.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
