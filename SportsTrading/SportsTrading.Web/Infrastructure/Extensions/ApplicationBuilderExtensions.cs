06d4b79d12899d1654a7dd4d20e645315ffd346c
ivanmanev26 <doiychevk@gmail.com>
2019-06-15 20:43:34 +0300

    Ädded Newtonsoft.Json and transformed json object to c# objects

--- SportsTrading/SportsTrading.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
+++ SportsTrading/SportsTrading.Web/Infrastructure/Extensions/ApplicationBuilderExtensions.cs
@@ -1,12 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json;
 using SportsTrading.Data;
+using SportsTrading.Data.Models;
 
 namespace SportsTrading.Web.Infrastructure.Extensions
 {
@@ -14,6 +17,9 @@ namespace SportsTrading.Web.Infrastructure.Extensions
     {
         public static IApplicationBuilder Seed(this IApplicationBuilder app)
         {
+            string jsonPath = @"C:\Users\Kalin\Desktop\sport-events-backup.json";
+            string json = File.ReadAllText(jsonPath);
+            List<Event> events = JsonConvert.DeserializeObject<List<Event>>(json);
             using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>().CreateScope())
             {
                 serviceScope.ServiceProvider.GetService<SportsTradingDbContext>().Database.Migrate();
